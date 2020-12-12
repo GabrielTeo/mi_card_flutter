9ae49c73f104bb5a3fb0325f22b0896606da5a63
GabrielTeo <gabrieltyx@gmail.com>
2020-12-13 01:18:30 +0800

    Edit Container Properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
